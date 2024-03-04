9ebacd9deb36a0f3546bacdbf8aaf801fffbc245
graymarhel <langgakolot@gmail.com>
2024-03-04 16:25:13 +0800

    first commit

--- activity2/resources/views/home.blade.php
+++ activity2/resources/views/home.blade.php
@@ -5,24 +5,59 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
+   
     <title>Bootstrap Example</title>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
+
+      <!-- fonts -->
+      <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
 
 
     <style>
          body{
         background-color: #222125;
+        font-family: "Merriweather", serif;
+        font-weight: 300;
+        font-family: "Radiance", "Noto Sans", sans-serif;
     }
 
 
-      .gongs1{
-        height:50vh;
-        display:flex;
-        color:white;
-        text-align: center;
-       
-      }
+    
+    main {
+      display: flex;
+      flex-direction: column-reverse;
+      box-sizing: border-box;
+      justify-content: center;
+      padding-top: 5rem;
+      
+    }
+
+    h1 {
+      align-self: center;
+      font-size: 5rem;
+      font-weight: 700;
+    }
+
+    p {
+      text-align: center;
+      font-size: 19px;
+      font-weight: 300;
+    }
+
+
+    .box{
+      justify-content:center ;
+      display:text;
+      padding:10px;
+
+    }
+
+  
+
+ 
 
 
 
@@ -87,21 +122,60 @@
       </div>
     </nav>
 
-    <div>
+    
 
-      <h1 class="gongs1">CHOOSE YOUR HERO</h1>
+  <main class="container text-white">
+
+    <p>From magical tacticians to fierce brutes and cunning rogues, Dota 2's hero pool is massive and limitlessly diverse. </br>  Unleash incredible abilities and devastating ultimates on your way to victory.</p>
+    <h1>CHOOSE YOUR HERO</h1>
+  </main>
+  
+  
+  </br></br></br>
+<!-- dota2 heroes -->
+
+  <!-- <div class="logo2"> 
+   <a class="heroes1" href="#">
+  <img class="logo" src="{{asset('images/abaddon.png')}}" alt="">&nbsp;
+  <img class="logo" src="{{asset('images/alchemist.png')}}" alt="">&nbsp;
+ <img class="logo" src="{{asset('images/ancient.png')}}" alt="" >&nbsp;
+ <img class="logo" src="{{asset('images/antimage.png')}}" alt="" >&nbsp;
+  <img class="logo" src="{{asset('images/arc.png')}}" alt="" >&nbsp;
+  
+
+  </div>
+
+  <div class="logo2"> 
+   <a class="heroes1" href="#">
+        <img class="logo" src="{{asset('images/axe.png')}}" alt="" >&nbsp;
+        <img class="logo" src="{{asset('images/bane.png')}}" alt="" >&nbsp;
+        <img class="logo" src="{{asset('images/bat.png')}}" alt="" >&nbsp;
+        <img class="logo" src="{{asset('images/beastmaster.png')}}" alt="" >&nbsp;
+        <img class="logo" src="{{asset('images/bloodseeker.png')}}" alt="" >      
+    </a>
+  </div> -->
+  <div class="logo2"> 
+    <div class="logo1">
+                <img class="box" src="images/abaddon.png" alt="">
+                <img class="box" src="images/alchemist.png" alt="">
+                <img class="box" src="images/ancient.png" alt="">
+                <img class="box" src="images/antimage.png" alt="">
+                <img class="box" src="images/arc.png" alt="">
+    </div> 
+  </div>    
+
+  <div class="logo2"> 
+    <div class="logo1">
+                <img class="box" src="images/axe.png" alt="">
+                <img class="box" src="images/bane.png" alt="">
+                <img class="box" src="images/bat.png" alt="">
+                <img class="box" src="images/beastmaster.png" alt="">
+                <img class="box" src="images/bloodseeker.png" alt="">
+    </div> 
+  </div>    
 
 
 
-
-
-
-
-
-
-
-
-    </div>
 
 
 
---
+++ activity2/resources/views/qwe.blade.php
@@ -0,0 +1,9 @@
+<div class="logo2"> 
+   <a class="heroes1" href="#">
+        <img class="logo" src="{{asset('images/axe.png')}}" alt="" width="200" height="120">
+        <img class="logo" src="{{asset('images/bane.png')}}" alt="" width="200" height="120">
+        <img class="logo" src="{{asset('images/bat.png')}}" alt="" width="200" height="120">
+        <img class="logo" src="{{asset('images/beastmaster.png')}}" alt="" width="200" height="120">
+        <img class="logo" src="{{asset('images/bloodseeker.png')}}" alt="" width="200" height="120">      
+    </a>
+  </div>
